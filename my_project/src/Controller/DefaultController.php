691a84321c745f9616250c3acd131b520d184f9e
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-11-07 15:13:14 -0300

    how to create database

--- my_project/src/Controller/DefaultController.php
+++ my_project/src/Controller/DefaultController.php
@@ -14,14 +14,13 @@ class DefaultController extends AbstractController
     public function index($name): Response
     {
         //? Return to a page with parameter to access from twig
-        // return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',]);
+        return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',]);
         //? Return a json string
         // return $this->json(['controller_name' => 'DefaultController',]);
         //? Redirect to a link
         // return $this->redirect('google.com');
          //? Redirect to a  Route with or not paramaters
         // return $this->redirectToRoute('default2');
-        return new Response("Hello {$name}");
     }
 
     /**
