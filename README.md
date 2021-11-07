e501ee84df3c7a060436465ae4501fb5dc29f7f9
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-11-07 15:33:25 -0300

    upd

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 #### Install
 - composer create-project symfony/skeleton my_project_name
 - php -S 127.0.0.1:8000 -t public
+
+#### Controllers return methods:
+    //? Return to a page with parameter to access from twig
+    return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',]);
+
+    //? Return a json string
+    // return $this->json(['controller_name' => 'DefaultController',]);
+    
+    //? Redirect to a link
+    // return $this->redirect('google.com');
+
+    //? Redirect to a  Route with or not paramaters
+    // return $this->redirectToRoute('default2');
+
+    //? Create a new response object
+    return new Response('from controller default 1');
+
+#### Models
--- my_project/src/Controller/DefaultController.php
+++ my_project/src/Controller/DefaultController.php
@@ -13,14 +13,7 @@ class DefaultController extends AbstractController
      */
     public function index($name): Response
     {
-        //? Return to a page with parameter to access from twig
-        return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',]);
-        //? Return a json string
-        // return $this->json(['controller_name' => 'DefaultController',]);
-        //? Redirect to a link
-        // return $this->redirect('google.com');
-         //? Redirect to a  Route with or not paramaters
-        // return $this->redirectToRoute('default2');
+        return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController']);
     }
 
     /**
@@ -28,7 +21,6 @@ class DefaultController extends AbstractController
      */
     public function index2(): Response
     {
-        //? Create a new response object
         return new Response('from controller default 1');
     }
 
