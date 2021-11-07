d6070d55d4d6c74bb4f0a49bbcb3215d4d17230b
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-11-07 15:02:29 -0300

    about returns in controller

--- my_project/src/Controller/DefaultController.php
+++ my_project/src/Controller/DefaultController.php
@@ -9,12 +9,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/default/{name}", name="default")
      */
-    public function index(): Response
+    public function index($name): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        //? Return to a page with parameter to access from twig
+        // return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',]);
+        //? Return a json string
+        // return $this->json(['controller_name' => 'DefaultController',]);
+        //? Redirect to a link
+        // return $this->redirect('google.com');
+         //? Redirect to a  Route with or not paramaters
+        // return $this->redirectToRoute('default2');
+        return new Response("Hello {$name}");
     }
+
+    /**
+     * @Route("/default2", name="default2")
+     */
+    public function index2(): Response
+    {
+        //? Create a new response object
+        return new Response('from controller default 1');
+    }
+
 }
