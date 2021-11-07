43373c8f2602626798d377b3cc3c2d653456a710
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-11-07 19:28:13 -0300

    controllers test

--- my_project/src/Controller/DefaultController.php
+++ my_project/src/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UsersRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
+use App\Entity\Users;
 
 class DefaultController extends AbstractController
 {
@@ -23,5 +27,29 @@ class DefaultController extends AbstractController
     {
         return new Response('from controller default 1');
     }
+
+    /**
+     * @Route("/user/create/{name}", name="user_create")
+     */
+    public function createUserDatabase(Request $request)
+    {
+        $name = $request->get('name');
+        $user = new Users();
+        $user->setName($name);
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('user_read');
+    }
+
+
+    /**
+     * @Route("/user/", name="user_read")
+     */
+    public function readUserDatabase(UsersRepository $user)
+    {
+        return $this->json($user->findAll());
+    }
+
 
 }
