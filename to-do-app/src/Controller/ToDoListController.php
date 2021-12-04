92180aab5172da19dfc645973e253181a9799c27
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-12-04 15:22:12 -0300

    task entity

--- to-do-app/src/Controller/ToDoListController.php
+++ to-do-app/src/Controller/ToDoListController.php
@@ -2,30 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
 {
     /**
-     * @Route("/to/do/list", name="to_do_list")
+     * @Route("/", name="to_do_list", methods={"GET"})
      */
     public function index(): Response
     {
-        return $this->render('to_do_list/index.html.twig', [
-            'controller_name' => 'ToDoListController',
-        ]);
+        return $this->render('index.html.twig');
     }
 
     /**
-     * @Route("/", name="to_do_list")
+     * @Route("/create", name="create_task", methods={"POST"})
      */
-    public function index2(): Response
+    public function create(Request $request): Response
     {
-        return $this->render('/index.html.twig', [
-            'controller_name' => 'ToDoListController',
-        ]);
+        $title = trim($request->request->get('title'));
+
+        if (empty($title)) return $this->redirectToRoute('to_do_list');
+
+        $task = new Task;
+        $task->setTitle($title);
+
+        $this->getDoctrine()->getManager()->persist($task);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status", methods={"GET"})
+     */
+    public function switchStatus($id)
+    {
+        exit('switch task...' . $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_task", methods={"GET"})
+     */
+    public function delete($id)
+    {
+        exit('delete task...' . $id);
     }
 
 }
---
+++ to-do-app/src/Entity/Task.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
