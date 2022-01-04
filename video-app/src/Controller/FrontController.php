210007592be79d28dd333803f3b2250e6ce6af23
Raziel Rodrigues <raziel@upoutsourcing.com>
2022-01-03 22:50:11 -0300

    changes

--- video-app/src/Controller/FrontController.php
+++ video-app/src/Controller/FrontController.php
@@ -8,11 +8,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/front", name="front")
+     * @Route("/", name="main_page")
      */
     public function index()
     {
         return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList()
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
     }
 }
 
