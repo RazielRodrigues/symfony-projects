cfa904673dbf8b084be71eef93aae2bc850fbecd
Raziel Rodrigues <raziel@upoutsourcing.com>
2022-01-04 00:28:58 -0300

    new entries

--- video-app/src/Controller/AdminController.php
+++ video-app/src/Controller/AdminController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -14,9 +13,50 @@ class AdminController extends AbstractController
     /**
      * @Route("/", name="admin_main_page")
      */
-    public function index(): Response
+    public function index()
     {
         return $this->render('admin/my_profile.html.twig');
     }
 
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories()
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategory()
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos()
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/upload-video", name="upload_video")
+     */
+    public function uploadVideo()
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users()
+    {
+        return $this->render('admin/users.html.twig');
+    }
 }
+
